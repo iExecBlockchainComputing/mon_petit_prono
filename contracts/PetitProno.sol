5bf16db73ac79627e96a75c949602ee915619dac
Robin <roleciq@gmail.com>
2022-10-10 22:57:09 +0200

    unitaire test Smart Contract

--- contracts/PetitProno.sol
+++ contracts/PetitProno.sol
@@ -1,29 +1,36 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.9;
 
-contract PetitProno{
+contract PetitProno {
     address public owner;
     string public League;
-    mapping (string => string) LeaguePool;
+    mapping(string => string) LeaguePool;
 
     constructor() {
         owner = msg.sender;
     }
 
-    modifier OnlyOwner(){
-        require (msg.sender == owner);
+    modifier OnlyOwner() {
+        require(msg.sender == owner);
         _;
     }
 
-    function get(string memory league_addr) public view returns (string memory) {
+    function get(string memory league_addr)
+        public
+        view
+        returns (string memory)
+    {
         return LeaguePool[league_addr];
     }
 
-    function set(string memory league_addr, string memory ipfs_link) public OnlyOwner {
+    function set(string memory league_addr, string memory ipfs_link)
+        public
+        OnlyOwner
+    {
         LeaguePool[league_addr] = ipfs_link;
     }
 
-    function remove(string memory league_addr) public OnlyOwner{
+    function remove(string memory league_addr) public OnlyOwner {
         delete LeaguePool[league_addr];
     }
 }
--- test/Lock.js
+++ test/Lock.js
@@ -1,126 +1,53 @@
-const {
-  time,
-  loadFixture,
-} = require("@nomicfoundation/hardhat-network-helpers");
-const { anyValue } = require("@nomicfoundation/hardhat-chai-matchers/withArgs");
-const { expect } = require("chai");
+const { loadFixture } = require('@nomicfoundation/hardhat-network-helpers')
+const { anyValue } = require('@nomicfoundation/hardhat-chai-matchers/withArgs')
+const { expect } = require('chai')
+const { ethers } = require('hardhat')
 
-describe("Lock", function () {
-  // We define a fixture to reuse the same setup in every test.
-  // We use loadFixture to run this setup once, snapshot that state,
-  // and reset Hardhat Network to that snapshot in every test.
-  async function deployOneYearLockFixture() {
-    const ONE_YEAR_IN_SECS = 365 * 24 * 60 * 60;
-    const ONE_GWEI = 1_000_000_000;
+describe('Mon petit prono test', function () {
+  async function deployTokenFixture() {
+    //contract are deployed using first signer account by default
+    const [owner, otherAccount] = await ethers.getSigners()
 
-    const lockedAmount = ONE_GWEI;
-    const unlockTime = (await time.latest()) + ONE_YEAR_IN_SECS;
+    const contract = await ethers.getContractFactory('PetitProno')
+    const deployedContract = await contract.deploy()
 
-    // Contracts are deployed using the first signer/account by default
-    const [owner, otherAccount] = await ethers.getSigners();
-
-    const Lock = await ethers.getContractFactory("Lock");
-    const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
-
-    return { lock, unlockTime, lockedAmount, owner, otherAccount };
+    return { deployedContract, owner, otherAccount }
   }
+  describe('Test deployment ', function () {
+    it('Should set right owner', async function () {
+      const { deployedContract, owner } = await loadFixture(deployTokenFixture)
+      expect(await deployedContract.owner()).to.equal(owner.address)
+    })
+  })
 
-  describe("Deployment", function () {
-    it("Should set the right unlockTime", async function () {
-      const { lock, unlockTime } = await loadFixture(deployOneYearLockFixture);
+  describe('Getter & Setter', function () {
+    it('Should set and get the right value', async function () {
+      const { deployedContract } = await loadFixture(deployTokenFixture)
+      await deployedContract.set(
+        '0xa1B1CAbE3FF10B0e08B95F74BF7A374A4A9f85d6',
+        'https://ipfs_link',
+      )
+      expect(
+        await deployedContract.get(
+          '0xa1B1CAbE3FF10B0e08B95F74BF7A374A4A9f85d6',
+        ),
+      ).to.equal('https://ipfs_link')
+    })
 
-      expect(await lock.unlockTime()).to.equal(unlockTime);
-    });
-
-    it("Should set the right owner", async function () {
-      const { lock, owner } = await loadFixture(deployOneYearLockFixture);
-
-      expect(await lock.owner()).to.equal(owner.address);
-    });
-
-    it("Should receive and store the funds to lock", async function () {
-      const { lock, lockedAmount } = await loadFixture(
-        deployOneYearLockFixture
-      );
-
-      expect(await ethers.provider.getBalance(lock.address)).to.equal(
-        lockedAmount
-      );
-    });
-
-    it("Should fail if the unlockTime is not in the future", async function () {
-      // We don't use the fixture here because we want a different deployment
-      const latestTime = await time.latest();
-      const Lock = await ethers.getContractFactory("Lock");
-      await expect(Lock.deploy(latestTime, { value: 1 })).to.be.revertedWith(
-        "Unlock time should be in the future"
-      );
-    });
-  });
-
-  describe("Withdrawals", function () {
-    describe("Validations", function () {
-      it("Should revert with the right error if called too soon", async function () {
-        const { lock } = await loadFixture(deployOneYearLockFixture);
-
-        await expect(lock.withdraw()).to.be.revertedWith(
-          "You can't withdraw yet"
-        );
-      });
-
-      it("Should revert with the right error if called from another account", async function () {
-        const { lock, unlockTime, otherAccount } = await loadFixture(
-          deployOneYearLockFixture
-        );
-
-        // We can increase the time in Hardhat Network
-        await time.increaseTo(unlockTime);
-
-        // We use lock.connect() to send a transaction from another account
-        await expect(lock.connect(otherAccount).withdraw()).to.be.revertedWith(
-          "You aren't the owner"
-        );
-      });
-
-      it("Shouldn't fail if the unlockTime has arrived and the owner calls it", async function () {
-        const { lock, unlockTime } = await loadFixture(
-          deployOneYearLockFixture
-        );
-
-        // Transactions are sent using the first signer by default
-        await time.increaseTo(unlockTime);
-
-        await expect(lock.withdraw()).not.to.be.reverted;
-      });
-    });
-
-    describe("Events", function () {
-      it("Should emit an event on withdrawals", async function () {
-        const { lock, unlockTime, lockedAmount } = await loadFixture(
-          deployOneYearLockFixture
-        );
-
-        await time.increaseTo(unlockTime);
-
-        await expect(lock.withdraw())
-          .to.emit(lock, "Withdrawal")
-          .withArgs(lockedAmount, anyValue); // We accept any value as `when` arg
-      });
-    });
-
-    describe("Transfers", function () {
-      it("Should transfer the funds to the owner", async function () {
-        const { lock, unlockTime, lockedAmount, owner } = await loadFixture(
-          deployOneYearLockFixture
-        );
-
-        await time.increaseTo(unlockTime);
-
-        await expect(lock.withdraw()).to.changeEtherBalances(
-          [owner, lock],
-          [lockedAmount, -lockedAmount]
-        );
-      });
-    });
-  });
-});
+    it('Should remove the right value', async function () {
+      const { deployedContract } = await loadFixture(deployTokenFixture)
+      await deployedContract.set(
+        '0xa1B1CAbE3FF10B0e08B95F74BF7A374A4A9f85d6',
+        'https://ipfs_link',
+      )
+      await deployedContract.remove(
+        '0xa1B1CAbE3FF10B0e08B95F74BF7A374A4A9f85d6',
+      )
+      expect(
+        await deployedContract.get(
+          '0xa1B1CAbE3FF10B0e08B95F74BF7A374A4A9f85d6',
+        ),
+      ).to.equal('')
+    })
+  })
+})
